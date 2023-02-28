2ac08a622d4687e5bdc5044c68fd69fa24d2d306
Mauricio Barbieri <42451192+mbarbierif@users.noreply.github.com>
2023-02-28 20:25:24 -0300

    Changes load method back to SQL

--- mercadolibre_pipeline_dag.py
+++ mercadolibre_pipeline_dag.py
@@ -49,7 +49,13 @@ def extract_and_transform_step():
         }
     )
 
-    df.to_csv("/home/airflow/gcs/data/daily_products.csv", header=True) # Saves product data as a CSV fiile in GCS
+    with open("/home/airflow/gcs/dags/sql/daily_products.sql", "w") as file:
+        file.write("")
+    
+    with open("/home/airflow/gcs/dags/sql/daily_products.sql", "a") as file:
+        for r in df.iterrows():
+            line = f"INSERT INTO products VALUES ('{r.id}','{r.site_id}','{r.title}','{r.price}','{r.sold_quantity}','{r.thumbnail}','{r.created_date}')\n"
+            file.write(line)
 
 # Block II: Definition of Airflow DAG and DB operations:
 with DAG(
@@ -61,7 +67,8 @@ with DAG(
     
     extract_and_transform_step = PythonOperator(
         task_id="extract_and_transform_step",
-        python_callable=extract_and_transform_step
+        python_callable=extract_and_transform_step,
+        retries=0
     )
 
     create_table_if = PostgresOperator(
@@ -76,13 +83,15 @@ with DAG(
             sold_quantity INT NOT NULL,
             thumbnail VARCHAR NOT NULL,
             created_date DATE NOT NULL);
-            '''
+            ''',
+        retries=0
     )
 
     load_step = PostgresOperator(
         task_id="load_step",
         postgres_conn_id="airflow_db",
-        sql="\copy products(id, site_id, title, price, sold_quantity, thumbnail, created_date) FROM '/home/airflow/gcs/data/daily_products.csv' DELIMITER ',' CSV HEADER"
+        sql="/home/airflow/gcs/dags/sql/daily_products.sql",
+        retries=0
     )
 
     extract_and_transform_step >> create_table_if >> load_step
