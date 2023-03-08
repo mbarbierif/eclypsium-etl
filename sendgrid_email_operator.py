46a9ae89421fe46678e1953af3fed457e37bee4e
Mauricio Barbieri <42451192+mbarbierif@users.noreply.github.com>
2023-03-08 14:48:58 -0300

    Adds sendgrid pipeline

---
+++ sendgrid_email_operator.py
@@ -0,0 +1,20 @@
+import datetime
+
+import airflow
+from airflow.operators.email import EmailOperator
+
+
+with airflow.DAG(
+    "composer_sample_sendgrid",
+    start_date=datetime.datetime(2023, 3, 7),
+) as dag:
+
+    task_email = EmailOperator(
+        task_id="send-email",
+        conn_id="sendgrid_default",
+        # You can specify more than one recipient with a list.
+        to="user@example.com",
+        subject="EmailOperator test for SendGrid",
+        html_content="This is a test message sent through SendGrid.",
+        dag=dag,
+    )
